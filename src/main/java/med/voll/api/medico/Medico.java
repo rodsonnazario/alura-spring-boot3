13ffb7faacbc9511caaa11ff99df4d31fa472659
rodsonnazario <rodson.nazario@gmail.com>
2023-01-24 23:49:53 -0300

    atualizando registro (PUT)

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
 
+import java.util.Optional;
+
 @Table(name = "medicos")
 @Entity(name = "Medico")
 @Getter
@@ -35,5 +37,14 @@ public class Medico {
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public void atualizar(DadosAtualizacaoMedico dados) {
+        if (dados.nome() != null)
+            this.nome = dados.nome();
+        if (dados.telefone() != null)
+            this.telefone = dados.telefone();
+        if (dados.endereco() != null)
+            this.endereco.atualizar(dados.endereco());
     }
 }
